a8432d4553c1ec84fb290076746ccb965843691e
Ubuntu <signaladmin@signal-v7-cds.x4llqqufycje1hmke4s2mglxgh.fx.internal.cloudapp.net>
2023-02-19 22:10:05 +0000

    Update build and start files

---
+++ start-debug-suspend.sh
@@ -0,0 +1,3 @@
+#/bin/bash
+java -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=*:8200 -jar ./service/target/contactdiscovery-1.65.jar server ../CDS_config.yml
+
--- start-debug.sh
+++ start-debug.sh
@@ -1,4 +1,3 @@
 #/bin/bash
-#java -jar ./service/target/contactdiscovery-1.53.jar server ../CDS_config.yml
-java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:8200 -jar ./service/target/contactdiscovery-1.53.jar server ../CDS_config.yml
+java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:8200 -jar ./service/target/contactdiscovery-1.65.jar server ../CDS_config.yml
 
